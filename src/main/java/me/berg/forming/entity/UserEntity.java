62e5a7f6ecc70bcd55c4fe0751cbbe5b5d4e10e1
berg-1 <884441972@qq.com>
2021-12-30 00:04:10 +0800

    JSON format LocalDateTime

--- src/main/java/me/berg/forming/entity/UserEntity.java
+++ src/main/java/me/berg/forming/entity/UserEntity.java
@@ -3,6 +3,7 @@ package me.berg.forming.entity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -68,6 +69,7 @@ public class UserEntity implements UserDetails {
      * 最后登录时间
      */
     @ApiModelProperty(value = "最后登录时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime lastLoginTime;
 
     @TableField(exist = false)
